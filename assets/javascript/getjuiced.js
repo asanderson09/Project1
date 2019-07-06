72f5c26ca1decb54cb96f8316595cb06eecd0499
Anthony A <asanderson2009@gmail.com>
2019-07-06 12:03:40 -0400

    removed init to fix console error for google not being defined

--- assets/javascript/getjuiced.js
+++ assets/javascript/getjuiced.js
@@ -1,27 +1,33 @@
 
-        var queryURL = "http://api.openchargemap.io/v3/poi/?output=json&countrycode=US&maxresults=10";
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            console.log(response[0].AddressInfo.Latitude)
-        
+var queryURL = "http://api.openchargemap.io/v3/poi/?output=json&countrycode=US&maxresults=10";
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+    console.log(response[0].AddressInfo.Latitude)
+
+});
+
+
+var queryURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=dog&format=json&origin=*";
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+
+
+});
+
+function initMap() {
+    var location = { lat: -25.363, lng: 131.044 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: location
     });
-
-
-    var queryURL = "https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=dog&format=json&origin=*";
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response);
-            
-        
-    });
-
-    
+}
+// initMap();
 
 
 
-    
